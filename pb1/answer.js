ca68f204c5345b727fccb659a82f11933fdc54f3
Clément S <csimonin.dev@outlook.fr>
2018-06-16 20:25:47 +0200

    formatting

--- pb1/answer.js
+++ pb1/answer.js
@@ -1,41 +1,41 @@
+
+
 // Time : 06:18
 const pb1 = ( list ) => {
-  if(list.length > 0) {
-    let product = 1;
-    for(let i=0;  i< list.length; i++) {
-      product *= list[i];
-    }
+  let product = 1;
+  
+  
+  for(let i=0;  i< list.length; i++) {
+    // Product of every element in the input array
+    product *= list[i];
+  }  
 
-    for(let i=0;  i< list.length; i++) {
-      list[i] = product / list[i];
-    }
-    return list;
+  for(let i=0;  i< list.length; i++) {
+    // Divide by current index and push
+    list[i] = product / list[i];
   }
-  return [];
+
+  return list;
 }
 
 console.log(pb1([1, 2, 3, 4, 5]));
 
 // Bonus Time : 02:34
 const pb1_bonus = ( list ) => {
-  if(list.length > 0) { 
-    const ret = [];
-    let product = 1;
-    for(let i=0;  i< list.length; i++) {
-      
-      for(let j=0;  j< list.length; j++) {
-        if(j !== i) 
-          product *= list[j];
-      }
-      
-      ret[i] = product;
-      product = 1;
+  const ret = [];
+  let product = 1;
+  for(let i=0;  i< list.length; i++) {
+    
+    for(let j=0;  j< list.length; j++) {
+      if(j !== i) 
+        product *= list[j];
     }
-
-    return ret;
-
+    
+    ret[i] = product;
+    product = 1;
   }
-  return [];
+
+  return ret;
 }
 
 console.log(pb1_bonus([1, 2, 3, 4, 5]));
