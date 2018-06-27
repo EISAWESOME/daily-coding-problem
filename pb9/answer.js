4ad3340b899c42736a7894605146ed11e534fbc9
Clément S <csimonin.dev@outlook.fr>
2018-06-27 21:24:19 +0200

    *

--- pb9/answer.js
+++ pb9/answer.js
@@ -4,8 +4,9 @@ const i2 = [5, 1, 1, 5];
 const nonAdjSum = (input) => {
   let incl = 0;
   let excl = 0;
+  let new_excl;
   input.forEach( (el) => {
-    let new_excl = excl > incl ? excl : incl;
+    new_excl = excl > incl ? excl : incl;
     incl = excl + el;
     excl = new_excl;
   })
