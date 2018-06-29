c94d159bbd36dde40df340deb98d89332aa91eeb
Clément S <csimonin.dev@outlook.fr>
2018-06-30 01:59:30 +0200

    *

--- pb13/answer.js
+++ pb13/answer.js
@@ -2,9 +2,10 @@
 
 const kdistinct = (string, k) => {
   const allSubString = [];
+  let longestSubstring = "";
 
   for(let i=0; i < string.length; i++) {
-    let currentSubString = "";
+    let currentSubString = "";    
     let distinctCounter = 0;
     for(let j=i; j < string.length; j++) {     
       if(!currentSubString.includes(string.charAt(j))) {
@@ -15,13 +16,14 @@ const kdistinct = (string, k) => {
       }
       currentSubString += string.charAt(j);  
     }
+    longestSubstring = currentSubString.length > longestSubstring.length ? currentSubString : longestSubstring;
     allSubString.push(currentSubString);
+
   }
-  return allSubString.reduce(function(a, b) { 
-    return a.length > b.length ? a : b
-  }, '');
+  console.log({ allSubString})
+  return longestSubstring;
 }
 
 const s = "abcba";
-const k = 2;
+const k =2;
 console.log(kdistinct(s, k))
